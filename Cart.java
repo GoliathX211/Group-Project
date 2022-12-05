436e2c60c61fbf592930bf51274f66675da5cffa
GoliathX211 <robertson0318@neo.rr.com>
2022-12-05 16:12:50 -0500

    Changes to Cart.java

--- Cart.java
+++ Cart.java
@@ -4,10 +4,20 @@ public class Cart {
         this.items = new Item[0];
     }
     public void addItem(Item item) {
-        Item[] newList = new Item[items.length + 1];
-        for (Item itemCopy : this.items) {
-            newList.clone()
+        Item[] newList = new Item[this.items.length + 1];
+        for (int index = 0; index < this.items.length; index++) {
+            newList[index] = this.items[index];
         }
-
+        newList[items.length] = item;
+        this.items = newList;
+    }
+    public void displayItems() {
+        Double totalCost = 0.00d;
+        System.out.println("Current Cart: \n");
+        for (int index = 0; index < this.items.length; index++) {
+            System.out.println("$" + this.items[index].cost + "\t" + this.items[index].name);
+            totalCost += this.items[index].cost;
+        }
+        System.out.println("\nTotal Cost:\n$" + totalCost);
     }
 }
